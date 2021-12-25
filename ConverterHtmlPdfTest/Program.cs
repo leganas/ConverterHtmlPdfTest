3dea5ab8b5ee62b1848bd05d3f40a13bda12469d
Andrei <leganas@gmail.com>
2021-12-26 01:23:28 +0300

    Update Program.cs

--- ConverterHtmlPdfTest/Program.cs
+++ ConverterHtmlPdfTest/Program.cs
@@ -10,7 +10,12 @@ namespace ConverterHtmlPdfTest
         {
             string[] files = Directory.GetFiles(@"D:\HTML_to_PDF\html", "*.html");
 //            var convertHelper = new PdfSharpBaseConvertHelper(); // Не потокобезопасен Гавно как оказалась, плюс глючит при конвертации
-//            var convertHelper = new ITextSharp_LGPL_Helper();  // Бесплатная версия iText , нужно разбираться как реализовать конвертацию
+            // Бесплатная версия iText , нужно разбираться как реализовать конвертацию
+            // В ней нет такого простого интерфейса как в uText7, т.к. это старая версия этой библиотеки
+            // В те времена когда она еще была бесплатной полностью, НО , она как пишут полность работает
+            // Я не нашёл примеров , нужно разбираться как реализовывалась там ранее конвертация
+            // Судя по коду который лежит на Github , она до сих  пор поддерживается , и там есть весь нужный функционал
+//            var convertHelper = new ITextSharp_LGPL_Helper();  
             var convertHelper = new iText7ConverterHelper(); // Работает хорошо но платная для комерческого использования, если узнают :) 
             convertHelper.ConvertMultiThread(files, @"D:\HTML_to_PDF\pdf");
 
